b1ac9e035471d3ba1fdc7d85e7d0c82a1b62cae3
Tamerlan1993 <tamerlan.piri@gmail.com>
2022-05-11 23:53:18 +0400

    dark mode changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { FaMoon } from "react-icons/fa";
-
+import React, { useState } from "react";
+import { BsMoon, BsMoonFill } from "react-icons/bs"
 function Header() {
+  const [darkMode, setDarkMode] = useState(false)
   return (
     <header className="flex justify-between px-16 py-4 items-center shadow-lg dark:bg-Dark_Blue">
       <h1 className="text-2xl font-bold">Where in the world?</h1>
       <p className="flex items-center gap-2 cursor-pointer" onClick={() => {
         document.documentElement.classList.toggle('dark')
+        setDarkMode(!darkMode)
       }}>
-        <FaMoon /> Dark Mode
+        {darkMode ? <BsMoon /> : <BsMoonFill />} Dark Mode
       </p>
     </header>
   );
